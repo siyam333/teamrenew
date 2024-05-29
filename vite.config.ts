6d16400908017dc1f0b992e7d595ba8d779a745c
siyam <siyamsundar475@gmail.com>
2024-05-29 20:39:55 +0530

    buildout

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/teamrenew/',
   plugins: [svelte()],
+  build: {
+    outDir: 'dist'
+  }
 })
